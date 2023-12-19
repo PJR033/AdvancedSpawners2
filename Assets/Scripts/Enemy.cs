9512e309df6544bb326b5f7668fd4957c387c73c
PJR033 <face_it03@mail.ru>
2023-12-19 21:19:22 +0300

    Init

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,18 @@
+using TMPro;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private Transform _target;
+
+    public void GetTarget(Transform target)
+    {
+        _target = target;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _target.position, _speed * Time.deltaTime);
+    }
+}
